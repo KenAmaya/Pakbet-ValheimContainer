5a03425db827b0ad38b2e872fb19d407b29c35f0
KenAmaya <ken.amaya@protonmail.ch>
2022-01-17 03:11:07 +1300

    Fedora: Add script to install Docker Engine

---
+++ DockerSetup/Fedora/1-docker-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "Installing the dnf-plugins-core package"
+dnf -y install dnf-plugins-core
+
+echo "Setting up the Docker repository:stable"
+dnf config-manager \
+	--add-repo \
+	https://download.docker.com/linux/fedora/docker-ce.repo
+
+echo "Installing the Docker Engine"
+dnf -y install docker-ce \
+	docker-ce-cli \
+	containerd.io
+
+echo "Starting Docker"
+systemctl start docker
+
+echo "Verifying Docker install. Running the hello-world image"
+docker run hello-world
+
