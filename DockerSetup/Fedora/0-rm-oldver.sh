b3b80a01d56746e93446b9f8c337f98643e27fa3
KenAmaya <ken.amaya@protonmail.ch>
2022-01-17 02:58:50 +1300

    Added script to remove old versions of Docker

---
+++ DockerSetup/Fedora/0-rm-oldver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Uninstalling older version of Docker"
+dnf remove docker \
+	docker-client \
+	docker-client-latest \
+	docker-common \
+	docker-latest \
+	docker-latest-logrotate \
+	docker-logrotate \
+	docker-selinux \
+	docker-engine-selinux \
+	docker-engine
