ffe451c7547d45e69d30ed74279003b8ad9e04f7
KenAmaya <ken.amaya@protonmail.ch>
2022-02-15 16:58:34 +1300

    Use docker-compose so the server can use the env file for server settings.

--- 4-start-valheimserver.sh
+++ 4-start-valheimserver.sh
@@ -10,17 +10,6 @@ fi
 echo "Starting the Docker Daemon"
 systemctl start docker
 
-echo "Starting the Valheim Server"
-docker run -d \
-        --name valheim-server \
-        --cap-add=sys_nice \
-        --stop-timeout 120 \
-	--restart unless-stopped \
-	-p 10800-10801:10800-10801/udp \
-        -v $HOME/valheim-server/config:/config \
-        -v $HOME/valheim-server/data:/opt/valheim \
-        -e SERVER_NAME="Ken's Server" \
-        -e WORLD_NAME="FirstWorld" \
-        -e SERVER_PASS="Hello there." \
-        -e SERVER_PORT="10800" \
-        ghcr.io/lloesche/valheim-server 
+echo "Deploying the Valheim server with docker-compose"
+mv ./valheim-server
+docker-compose up
